cc2887c1cd257a5f01a2216af800ec98c438c5c7
mujhid4 <mujahid@vfairs.com>
2022-10-08 20:26:34 +0500

    first project

---
+++ src/Profile.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export class Profile extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Class Component</h1>  
+            </div>   
+        )
+    }
+}
+
+export class UserName extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Usernam Component</h1>  
+            </div>   
+        )
+    }
+}
