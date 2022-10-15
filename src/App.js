3f5ae7f6a60742c0f800c21768a69d69abd53faa
mujhid4 <mujahid@vfairs.com>
2022-10-15 12:33:16 +0500

    react tutorial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home'
+
+function App() {
+  return (
+    <div className="App">
+     <Home channel="Code With Mujahid"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,8 @@
+function Home(props) {
+    console.log(props)
+    return(
+        <h1>{props.channel}</h1>
+    )
+}
+
+export default Home
