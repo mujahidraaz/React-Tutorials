ab03a1f510ad0a57269218e1cd606658e2eade9c
mujhid4 <mujahid@vfairs.com>
2022-11-10 21:17:51 +0500

    materi al-ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Rating } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>React Material UI</h1>
+      <button onClick={() => {alert('lorem ipsum')}}>Click Me</button>
+      <Button onClick={() => {alert('lorem ipsum')}} variant="contained">Click Me</Button>
+      <Button variant="outlined">Outlined</Button>
+
+      <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+      <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import Profile from './Profile'
-// import {Profile, UserName} from './Profile'
 
 import User from './User'
 
@@ -12,10 +10,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App />
-    <Profile />
-    <UserName /> */}
-    <User />
+    <App />
 
 
 
