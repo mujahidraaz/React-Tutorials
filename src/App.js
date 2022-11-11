b353d6cc219931caeaeff185b072b03a12416829
mujhid4 <mujahid@vfairs.com>
2022-11-11 20:17:06 +0500

    change input field value

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState('React Form Input')
+
+  function onChange(val) {
+    // console.log(val.target.value)
+    setData(val.target.value)
+
+    
+  }
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>{data}</h1>
+      <input placeholder='Full Name' onChange={onChange}/>
+
     </div>
   );
 }
