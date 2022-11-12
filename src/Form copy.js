9ff4bfc5b513db4e32fbe424cb821c5590460194
mujhid4 <mujahid@vfairs.com>
2022-11-13 01:57:03 +0500

    form

--- src/Form copy.js
+++ src/Form copy.js
@@ -2,30 +2,29 @@ import { useState } from "react"
 
 function Form() {
 
-    const [name, setName] = useState('Rahul')
-    const [email, setEame] = useState('rahul@gmail.com')
-    const [sClass, setCame] = useState('Class 1')
-    const [term, setTerm] = useState(true)
-
-    function FormHandling(e) {
+    const [name, setName] = useState ('')
+    const [email, setEmail] = useState ('')
+    const [sClass, setClass] = useState ('')
+    const [term, setTerm] = useState (true)
+    function  FormHandling(e) {
         e.preventDefault()
         console.log(name, email, sClass, term)
     }
 
     return (
         <div>
-            <h1>React Form Handlig #22</h1>
+            <h1>React Form Styling</h1>
             <div className="form">
                 <form onSubmit={FormHandling}>
-                    <input type='text' placeholder="full name" onChange={(e) => setName(e.target.value)}/>
-                    <input type='email' placeholder="email" onChange={(e) => setEame(e.target.value)}/>
-                    <select onChange={(e) => setCame(e.target.value)}>
+                    <input type='text' placeholder="full name"  name="name" onChange={(e) => setName(e.target.value)} value={name}/>
+                    <input type='email' placeholder="email" onChange={(e) => setEmail(e.target.value)} value={email}/>
+                    <select onChange={(e) => setClass(e.target.value)} value={sClass}>
                         <option>Class 1</option>
                         <option>Class 2</option>
                         <option>Class 3</option>
                         <option>Class 4</option>
                     </select>
-                    <input type="checkbox" onChange={(e) => setTerm(e.target.value)}/><span>Term and condition</span>
+                    <input type="checkbox"  onChange={(e) => setTerm(e.target.value)} /><span>Term and condition</span>
                     <input type="submit" value="Submit" />
                 </form>
                 <div className="output">
@@ -34,7 +33,7 @@ function Form() {
                         <li><span>Full Name:</span> {name}</li>
                         <li><span>Email:</span> {email}</li>
                         <li><span>Class:</span> {sClass}</li>
-                        <li><span>Term and Condition:</span>{term}</li>
+                        <li><span>Term and Condition:</span> {term}</li>
                     </ul>
                 </div>
             </div>
