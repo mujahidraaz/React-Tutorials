1d81311be8ec01c8aa977b9606ffe9d70887cd91
mujhid4 <mujahid@vfairs.com>
2022-11-14 20:46:33 +0500

    form-validations

--- src/Form.js
+++ src/Form.js
@@ -2,23 +2,44 @@ import { useState } from "react"
 
 function Form() {
 
-    const [name, setName] = useState('Rahul')
-    const [email, setEame] = useState('rahul@gmail.com')
-    const [sClass, setCame] = useState('Class 1')
-    const [term, setTerm] = useState(true)
+    const [name, setName] = useState('')
+    const [email, setEame] = useState('')
+    const [sClass, setCame] = useState('')
+    const [term, setTerm] = useState('')
+
+    const [nameError, setNameError] = useState(false)
+    const [emailError, setEmailError] = useState(false)
+    const [termError, setTermError] = useState(false)
 
     function FormHandling(e) {
         e.preventDefault()
         console.log(name, email, sClass, term)
+        if(name.length == 0){
+            setNameError(true)
+        }else {
+            setNameError(false)
+        }
+        if(email.length == 0){
+            setEmailError(true)
+        }else {
+            setEmailError(false)
+        }
+        if(term.length == 0){
+            setTermError(true)
+        }else {
+            setTermError(false)
+        }
     }
 
     return (
         <div>
-            <h1>React Form Handlig #22</h1>
+            <h1>React Form Validations #23</h1>
             <div className="form">
                 <form onSubmit={FormHandling}>
                     <input type='text' placeholder="full name" onChange={(e) => setName(e.target.value)}/>
+                    {nameError?<label>please fill the full name</label>:""}
                     <input type='email' placeholder="email" onChange={(e) => setEame(e.target.value)}/>
+                    {emailError?<label>please fill the email</label>:""}
                     <select onChange={(e) => setCame(e.target.value)}>
                         <option>Class 1</option>
                         <option>Class 2</option>
@@ -26,6 +47,7 @@ function Form() {
                         <option>Class 4</option>
                     </select>
                     <input type="checkbox" onChange={(e) => setTerm(e.target.value)}/><span>Term and condition</span>
+                    {termError?<label>please fill the term</label>:""}
                     <input type="submit" value="Submit" />
                 </form>
                 <div className="output">
