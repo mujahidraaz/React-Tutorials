dfe89011deedc79ca6f185249a2b796598fe7089
mujhid4 <mujahid@vfairs.com>
2022-11-22 20:21:14 +0500

    useEffect Hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, {useEffect, useState} from 'react';
+
 function App() {
+
+  const [counter, setCounnter] = useState(0)
+  useEffect(() => {
+    setTimeout(() => {
+      setCounnter(counter+1)
+    }, 1000)
+  })
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>useEffect Hooks</h1>  
+      <h1>Counter: <span>{counter}</span></h1>  
     </div>
   );
 }
