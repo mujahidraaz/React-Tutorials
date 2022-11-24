4393ad52950190a668bc12e7930ecea7faa15543
mujhid4 <mujahid@vfairs.com>
2022-11-24 20:54:56 +0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import './App.css';
 
 import React, {useEffect, useState} from 'react';
 
-function App() {
+function App(props) {
 
   const [counter, setCounnter] = useState(0)
   useEffect(() => {
-    setTimeout(() => {
-      setCounnter(counter+1)
+
+    let timer = setTimeout(() => {
+      setCounnter(counter + 1)
+      console.log(props.channel)
     }, 1000)
-  })
+    return() => clearTimeout(timer)
+
+  }, [counter, props.channel])
 
   return (
     <div className="App">
-      <h1>useEffect Hooks</h1>  
+      <h1>useEffect Hooks (Dependency & Cleanup)</h1>  
+      <h1>{props.channel}</h1>
       <h1>Counter: <span>{counter}</span></h1>  
+      
     </div>
   );
 }
