228ce4bb7c723382e9abd3fea0f16e0d6ef7fd7c
mujhid4 <mujahid@vfairs.com>
2022-12-07 02:21:04 +0500

    usememo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import Counter from './Counter'
-import Timer from './Timer'
 
-import React, {createContext, useState, useEffect} from 'react';
-
-export const useCounter = createContext()
+import {useState, useEffect, useMemo} from 'react'
 
 function App() {
-  const [counter, setCouter] = useState(0)
+
+  const [counter, setCounter] = useState(0)
+  const [counter2, setCounter2] = useState(0)
+  const [counter3, setCounter3] = useState(0)
 
   useEffect(() => {
     setTimeout(() => {
-      setCouter(counter+1)
+      setCounter(counter+1)
     }, 1000)
   })
- 
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCounter2(counter2+1)
+    }, 3000)
+  })
+  
+  useMemo(() => {
+    setTimeout(() => {
+      setCounter3(counter3+1)
+    }, 1000)
+  }, [counter2])
+
   return (
-    <useCounter.Provider value={counter}>
-         <h1>useContext Hooks API</h1>
-         <Counter />
-         <Timer />
-    </useCounter.Provider>
+      <div className='app'>
+         <h1>useMemo Hook #30</h1>
+         <h1>{counter}</h1>
+         <h1>{counter2}</h1>
+         <h1>{counter3}</h1>
+      </div>
   );
   
 }
